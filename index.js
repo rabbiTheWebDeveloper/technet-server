02b78051391e4db991cb883d67ef1eff195af6f0
S M AL RABBI <95512554+rabbiTheWebDeveloper@users.noreply.github.com>
2023-07-08 16:55:01 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://nextLevel:nextLevel@cluster0.t8s5g.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
